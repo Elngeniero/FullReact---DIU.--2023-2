37465bd4c769db936edfaa0cd54e556d58b4d1f6
Pablo_Campos_lt <72324396+Elngeniero@users.noreply.github.com>
2023-10-22 20:24:33 -0300

    Agregue la ruta para la pag reserva.js

--- src/components/app_router.js
+++ src/components/app_router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HomePage from '../pages/home'
 import { PayPage } from '../pages/pagos'
+import ReservaPage from '../pages/reserva'
 
 
 
@@ -12,6 +13,7 @@ const AppRouter = () => {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/pagos' element={<PayPage />} />
+        <Route path='/reserva' element={<ReservaPage />} />
       </Routes>
     
     </BrowserRouter>
